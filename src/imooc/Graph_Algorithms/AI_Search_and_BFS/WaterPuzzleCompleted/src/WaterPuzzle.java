c787262e04b7c66324bc4f2b401facb5306ae42b
67ng <2279142891@qq.com>
2020-02-07 00:34:21 +0800

    倒水问题

--- src/imooc/Graph_Algorithms/AI_Search_and_BFS/WaterPuzzleCompleted/src/WaterPuzzle.java
+++ src/imooc/Graph_Algorithms/AI_Search_and_BFS/WaterPuzzleCompleted/src/WaterPuzzle.java
@@ -2,7 +2,10 @@ package imooc.Graph_Algorithms.AI_Search_and_BFS.WaterPuzzleCompleted.src;
 
 import java.util.*;
 import java.util.ArrayList;
-
+/**
+ *
+ * 用容积为5,3的两个桶子，量出4的水
+ * */
 public class WaterPuzzle {
 
     private int[] pre;
@@ -12,26 +15,26 @@ public class WaterPuzzle {
 
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[100];
-        pre = new int[100];
+        pre = new int[100];//记录倒水的步骤
 
         queue.add(0);
         visited[0] = true;
         while (!queue.isEmpty()){
             int cur = queue.remove();
             int a = cur / 10, b = cur % 10;
-            // max a = 5, max b = 3
+            // max a = 5, max b = 3,状态压缩：用一个两位数表示两个桶的一个状态,妙啊！
 
             ArrayList<Integer> nexts = new ArrayList<>();
-            nexts.add(5 * 10 + b);
-            nexts.add(a * 10 + 3);
-            nexts.add(a * 10 + 0);
-            nexts.add(0 * 10 + b);
+            nexts.add(5 * 10 + b);//给a桶灌满水
+            nexts.add(a * 10 + 3);//给b桶灌满水
+            nexts.add(a * 10 + 0);//b桶倒空
+            nexts.add(0 * 10 + b);//a桶倒空
 
-            int x = Math.min(a, 3 - b);
-            nexts.add((a - x) * 10 + (b + x));
+            int x = Math.min(a, 3 - b);//最多能倒的水
+            nexts.add((a - x) * 10 + (b + x));//a桶中的水倒进b桶
 
             int y = Math.min(b, 5 - a);
-            nexts.add((a + y) * 10 + (b - y));
+            nexts.add((a + y) * 10 + (b - y));//b桶中的水倒进a桶
 
             for(int next: nexts)
                 if(!visited[next]){
@@ -39,7 +42,7 @@ public class WaterPuzzle {
                     visited[next] = true;
                     pre[next] = cur;
 
-                    if(next / 10 == 4 || next % 10 == 4) {
+                    if(next / 10 == 4 || next % 10 == 4) {//后一种情况其实永远达不到
                         end = next;
                         return;
                     }
@@ -63,6 +66,7 @@ public class WaterPuzzle {
     }
 
     public static void main(String[] args){
+
         System.out.println((new WaterPuzzle()).result());
     }
 }
