da8eee9cd6791498bbc614df88b7cd11c51f0a8c
67ng <2279142891@qq.com>
2020-03-18 00:05:11 +0800

    栈和队列的补充、一些多叉树的数据结构

---
+++ src/simple_fun/data_structure/tree/multiway_tree/segment_tree/Solution1.java
@@ -0,0 +1,35 @@
+package simple_fun.data_structure.tree.multiway_tree.segment_tree;
+
+/**
+ * Description:应用线段树解决
+ *
+ * @date: 2018/11/27 22:15
+ */
+public class Solution1 {
+    /// 303. Range Sum Query - Immutable
+/// https://leetcode.com/problems/range-sum-query-immutable/description/
+
+    class NumArray {
+
+        private SegmentTree<Integer> segmentTree;
+
+        public NumArray(int[] nums) {
+
+            if (nums.length > 0) {
+                Integer[] data = new Integer[nums.length];
+                for (int i = 0; i < nums.length; i++)
+                    data[i] = nums[i];
+                segmentTree = new SegmentTree<>(data, (a, b) -> a + b);
+            }
+
+        }
+
+        public int sumRange(int i, int j) {
+
+            if (segmentTree == null)
+                throw new IllegalArgumentException("Segment Tree is null");
+
+            return segmentTree.query(i, j);
+        }
+    }
+}
---
+++ src/simple_fun/data_structure/tree/multiway_tree/trie/Main.java
@@ -0,0 +1,59 @@
+package simple_fun.data_structure.tree.multiway_tree.trie;
+
+/**
+ * Description:
+ *
+ * @date: 2018/11/28 19:49
+ */
+
+import _courses.imooc.datastructure.Set_Map.Set.BSTSet;
+import _courses.imooc.datastructure.Set_Map.Set.FileOperation;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("D:\\视频\\算法\\imooc算法\\算法1：玩转数据结构 从入门到进阶\\源码\\07-Set-and-Map\\02-LinkedListSet\\pride-and-prejudice.txt", words)) {
+
+            long startTime = System.nanoTime();
+
+            BSTSet<String> set = new BSTSet<>();
+            for (String word : words)
+                set.add(word);
+
+            for (String word : words)
+                set.contains(word);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("BSTSet: " + time + " s");
+
+            // ---
+
+            startTime = System.nanoTime();
+
+            Trie trie = new Trie();
+            for (String word : words)
+                trie.add(word);
+
+            for (String word : words)
+                trie.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + trie.getSize());
+            System.out.println("Trie: " + time + " s");
+        }
+    }
+}
+
