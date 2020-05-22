779fd4595a5bc5ea88fcc39697bc8f3313bada14
67ng <2279142891@qq.com>
2020-05-22 16:24:18 +0800

    子数组的数量

--- src/simple_fun/algorithm/DP/state_compression_dp/Solution.java
+++ src/simple_fun/algorithm/DP/state_compression_dp/Solution.java
@@ -33,7 +33,7 @@ public class Solution {
      */
 
     //回溯法
-    // 时间复杂度：O(k)。k 是有效排列的数目。
+    //时间复杂度：O(k)。k 是有效排列的数目。
     //空间复杂度：O(n)。使用了大小为 n 的数组 visited。递归树的深度最多为 n，这里 n 是给定的整数 n。
     int count = 0;
     public int countArrangement(int N) {
