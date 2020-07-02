ba47f6cc901ef6e083939140a767e93e638b6acb
Pythagodzilla <2279142891@qq.com>
2020-07-02 17:43:15 +0800

    汉明重量的算法性能测试结果差异

--- src/simple_fun/algorithm/math/bit_operation/HammingWeight.java
+++ src/simple_fun/algorithm/math/bit_operation/HammingWeight.java
@@ -7,9 +7,15 @@ package simple_fun.algorithm.math.bit_operation;
  */
 
 public class HammingWeight {
+
+    //    int start = -1000000000;
+//    int end = 1000;
+//    hammingWeight1: 16
+//    hammingWeight2: 7407
+//    hammingWeight3: 0
     // you need to treat n as an unsigned value
     //循环和位移动
-    public int hammingWeight(int n) {
+    public int hammingWeight1(int n) {
         int bits = 0;
         int mask = 1;
         for (int i = 0; i < 32; i++) {
@@ -22,6 +28,7 @@ public class HammingWeight {
     }
 
     //位运算,原理:对于任意数字 n ，将 n 和 n - 1 做与运算，会把最后一个 1 的位变成 0
+    //数据太大时，性能不好
     public int hammingWeight2(int n) {
         int sum = 0;
         while (n != 0) {
@@ -40,6 +47,58 @@ public class HammingWeight {
         n = n + (n >>> 16);
 
         return n & 0x3f;
+    }
+
+    public static void main(String[] args) {
+        HammingWeight hw = new HammingWeight();
+        long time1 = System.currentTimeMillis();
+//        int start = Integer.MIN_VALUE;
+//        int end = Integer.MAX_VALUE;
+
+        int start = 0;
+        int end = 1_000_000;
+        //hammingWeight1: 16ms
+        //hammingWeight2: 0ms
+        //hammingWeight3: 0ms
+
+        //int start = 0;
+        //int end = 10_000_000;
+        //hammingWeight1: 9ms
+        //hammingWeight2: 51ms
+        //hammingWeight3: 3ms
+
+        //int start = 0;
+        //int end = 100_000_000;
+        //hammingWeight1: 15ms
+        //hammingWeight2: 611ms
+        //hammingWeight3: 0ms
+
+        //int start = 0;
+        //int end = 1000_000_000;
+        //hammingWeight1: 16ms
+        //hammingWeight2: 6567ms
+        //hammingWeight3: 3ms
+
+
+        for (int i = start; i < end; i++) {
+            hw.hammingWeight1(i);
+        }
+        long time2 = System.currentTimeMillis();
+        System.out.println("hammingWeight1: " + (time2 - time1) + "ms");
+
+        long time3 = System.currentTimeMillis();
+        for (int i = start; i < end; i++) {
+            hw.hammingWeight2(i);
+        }
+        long time4 = System.currentTimeMillis();
+        System.out.println("hammingWeight2: " + (time4 - time3) + "ms");
+
+        long time5 = System.currentTimeMillis();
+        for (int i = start; i < end; i++) {
+            hw.hammingWeight3(i);
+        }
+        long time6 = System.currentTimeMillis();
+        System.out.println("hammingWeight3: " + (time6 - time5) + "ms");
     }
 
 
