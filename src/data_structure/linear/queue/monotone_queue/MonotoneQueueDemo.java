132707b7a2cbbf4b342568ed40dd09e72d6962a7
matreeix <2279142891@qq.com>
2021-06-05 23:35:25 +0800

    单调队列详解

--- src/data_structure/linear/queue/monotone_queue/MonotoneQueueDemo.java
+++ src/data_structure/linear/queue/monotone_queue/MonotoneQueueDemo.java
@@ -2,10 +2,10 @@ package data_structure.linear.queue.monotone_queue;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 
 /**
  * @Description: 单调队列
- *
  * @Link: https://leetcode-cn.com/problems/sliding-window-maximum/solution/dan-diao-dui-lie-by-labuladong/
  * @Author: matreeix
  * @Date: 2020/9/10
@@ -63,5 +63,43 @@ public class MonotoneQueueDemo {
         }
         return ans;
     }
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        //创建双端队列
+        Deque<Integer> deque = new ArrayDeque<>();
+        //先初始化前K个元素
+        for (int i = 0; i < k; i++) {
+            //判断队列是否为空 或者当前入队元素是否大于队尾元素 大于则出队
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            //当前元素入队
+            //由于需要判断当前元素是否在窗口中，所以实际上队列中存储的为当前元素的下标
+            //根据下标找元素比根据元素找下标方便
+            deque.offerLast(i);
+        }
+        int[] ans = new int[n - k + 1];
+        //添加当前最大元素
+        ans[0] = nums[deque.peekFirst()];
+
+        for (int i = k; i < n; i++) {
+            //判断队列是否为空 或者当前入队元素是否大于队尾元素 大于则出队
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            //当前元素入队
+            deque.offerLast(i);
+            //判断队首元素是否在窗口中
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            //添加答案
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+
+        return ans;
+    }
+
 
 }
